7ae3b2d1953b02b16f3ae1cf44c026df5d7e12af
Muhammad-Mehdi-Raza <bsef12m543@pucit.edu.pk>
2015-10-31 13:57:50 +0500

    1st

---
+++ FYP/FYP/Controllers/homeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FYP.Models;
+using System.Data.Mapping;
+
+namespace FYP.Controllers
+{
+    public class homeController : Controller
+    {
+        CMSdbEntities dbEntity = new CMSdbEntities();
+        //
+        // GET: /home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult login()
+        {
+            return View();
+        }
+        public ActionResult transfer()
+        {
+            string userId = Request["userId"];
+            string pwd = Request["pwd"];
+            user u = dbEntity.users.First(n => n.password.Equals(pwd) );
+            return View();
+        }
+
+    }
+}
